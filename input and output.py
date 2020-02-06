8fbe200a3cb83670394e996f13fd7876e4fd5792
root <ashithosh77@gamil.com>
2020-02-06 14:07:47 -0500

    2020_02_06_21_07

---
+++ input and output.py
@@ -0,0 +1,7 @@
+name = input("Enter name: ")
+print("Hello", name)
+
+a = input("enter somthing like (what are doing): ")
+print(a)
+print("I'm hacking world")
+
---
+++ type conversion.py
@@ -0,0 +1,15 @@
+# Implicit
+number_integer = 123
+number_float   = 1.23
+
+# adding interger and float
+
+sum = number_integer + number_float
+print(sum)
+print("data type of sum: ", type(sum))
+
+# let's try one more example
+a = input("enter somthing: ")
+print("type of a: ", type(a))
+
+# Explicit
